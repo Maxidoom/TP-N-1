ab71eee085a189f85ba6cd382b95c40f85aa2d5f
Lavergilsilla <ninjax06112009@gmail.com>
2025-05-01 17:35:56 -0300

    tp1 punto 2 i.cpp

--- tp1 punto 2 i.cpp
+++ tp1 punto 2 i.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-	int lado;
-	int perimetro;	
-cout<<"este programa va a calcular el perimetro de un cuadrado"<<endl;
-cout<<"ingrese el valor de un lado"<<endl;
-cin>>lado;
-	perimetro=lado*4;
-cout<<"el perimetro de tu un cuadrado es de "<<perimetro;
+	int lado; //ingresa la vaiable del lado
+	int perimetro;	//ingresa la variable del perimetro
+cout<<"este programa va a calcular el perimetro de un cuadrado"<<endl;//le indica al usuario que hace este programa
+cout<<"ingrese el valor de un lado"<<endl;//le pide al usuario que ingrese el valor del lado
+cin>>lado;//guarda el valo del usuario
+	perimetro=lado*4;//indica a que es igual la variable perimetro
+cout<<"el perimetro de tu un cuadrado es: "<<perimetro;//muestra el resultado del perimetro
     
     return 0;
 }
